aa82e0dee4005bdf3f5b4bf5be2066a4f91b79fb
beichenhpy <beichenhpy@outlook.com>
2021-06-19 15:55:06 +0800

    feat：init optional

---
+++ javaSe/src/main/java/cn/beichenhpy/basic/jdk8_new/lambda/OptionalTest.java
@@ -0,0 +1,16 @@
+package cn.beichenhpy.basic.jdk8_new.lambda;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author beichenhpy
+ * @version 0.0.1
+ * @apiNote OptionalTest description：Optional相关api测试
+ * @since 2021/6/9 21:15
+ */
+public class OptionalTest {
+    @Test
+    public void test1(){
+
+    }
+}
